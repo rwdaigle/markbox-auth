02b83739029082000553e8582e6defbf64d5baaa
Ryan Daigle <ryan.daigle@gmail.com>
2015-05-22 18:46:23 -0400

    Default to my access token

--- app/controllers/api/v1/domains_controller.rb
+++ app/controllers/api/v1/domains_controller.rb
@@ -3,13 +3,8 @@ module Api
     class DomainsController < ApiController
 
       def access_token
-        domain = params[:id]
-        if(domain == "ryandaigle.com")
-          user = User.find_by_db_user_id("264878654")
-          render json: { access_token: user.db_access_token }
-        else
-          render json: { access_token: nil }, status: 404
-        end
+        user = User.find_by_db_user_id("264878654")
+        render json: { access_token: user.db_access_token }
       end
     end
   end
